44172cf4f04f5bf57be549f01420459bbc8f25a0
abdulrehmanshabbir25-tech <abdulrehman.shabbir.25@ibadia.cat>
2026-05-31 01:10:04 +0200

    Commit 2: Aplicar patró 'Replace Magic Number with Symbolic Constant'
    - S'han eliminat els literals numèrics fixos (magic numbers) del mètode.\n- S'han definit constants amb noms significatius: MAX_QUALITAT (50), QUALITAT_MINIMA (0), LIMIT_DIES_URGENT (11) i LIMIT_DIES_CRITIC (6).\n- Centralitza la configuració del negoci i millora l'autoexplicació del codi.

--- Magatzem.java
+++ Magatzem.java
@@ -1,9 +1,16 @@
 /**
- * CODI LLEGAT (Legacy Code): Conté l'algorisme original complex amb un niuament elevat d'if/else.
- * Es puja completament intacte sense refactoritzar per servir com a punt de partida a Git.
+ * CODI REFACTORITZAT - COMMIT 2
+ * Patró aplicat: Replace Magic Number with Symbolic Constant.
+ * Es defineixen constants amb nom per eliminar els literals numèrics fixos.
  */
 class Magatzem {
     Article[] articles;
+
+    // PATRÓ: Replace Magic Number with Symbolic Constant
+    public static final int MAX_QUALITAT = 50;
+    public static final int QUALITAT_MINIMA = 0;
+    public static final int LIMIT_DIES_URGENT = 11;
+    public static final int LIMIT_DIES_CRITIC = 6;
 
     public Magatzem(Article[] articles) {
         this.articles = articles;
@@ -13,24 +20,24 @@ class Magatzem {
         for (int i = 0; i < articles.length; i++) {
             if (!articles[i].nom.equals("Formatge Gidurat")
                     && !articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                if (articles[i].qualitat > 0) {
+                if (articles[i].qualitat > QUALITAT_MINIMA) {
                     if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
                         articles[i].qualitat = articles[i].qualitat - 1;
                     }
                 }
             } else {
-                if (articles[i].qualitat < 50) {
+                if (articles[i].qualitat < MAX_QUALITAT) {
                     articles[i].qualitat = articles[i].qualitat + 1;
 
                     if (articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                        if (articles[i].diesPerVendre < 11) {
-                            if (articles[i].qualitat < 50) {
+                        if (articles[i].diesPerVendre < LIMIT_DIES_URGENT) {
+                            if (articles[i].qualitat < MAX_QUALITAT) {
                                 articles[i].qualitat = articles[i].qualitat + 1;
                             }
                         }
 
-                        if (articles[i].diesPerVendre < 6) {
-                            if (articles[i].qualitat < 50) {
+                        if (articles[i].diesPerVendre < LIMIT_DIES_CRITIC) {
+                            if (articles[i].qualitat < MAX_QUALITAT) {
                                 articles[i].qualitat = articles[i].qualitat + 1;
                             }
                         }
@@ -45,7 +52,7 @@ class Magatzem {
             if (articles[i].diesPerVendre < 0) {
                 if (!articles[i].nom.equals("Formatge Gidurat")) {
                     if (!articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                        if (articles[i].qualitat > 0) {
+                        if (articles[i].qualitat > QUALITAT_MINIMA) {
                             if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
                                 articles[i].qualitat = articles[i].qualitat - 1;
                             }
@@ -54,7 +61,7 @@ class Magatzem {
                         articles[i].qualitat = articles[i].qualitat - articles[i].qualitat;
                     }
                 } else {
-                    if (articles[i].qualitat < 50) {
+                    if (articles[i].qualitat < MAX_QUALITAT) {
                         articles[i].qualitat = articles[i].qualitat + 1;
                     }
                 }
