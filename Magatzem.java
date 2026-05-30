55e9bd12bbffbb8a060b78e57c8e5550c8d6eacc
abdulrehmanshabbir25-tech <abdulrehman.shabbir.25@ibadia.cat>
2026-05-31 01:16:05 +0200

    Commit 3: Aplicar patró 'Guard Clauses' per a l'article llegendari
    - S'ha introduït una clàusula de guarda al principi del bucle for per comprovar si l'article és el 'Martell de Thor (Llegendari)'.\n- Com que aquest objecte és immutable, s'aplica un 'continue' immediat.\n- Això redueix el nivell de niuament del mètode principal, eliminant condicions repetitives en els passos posteriors.

--- Magatzem.java
+++ Magatzem.java
@@ -1,12 +1,11 @@
 /**
- * CODI REFACTORITZAT - COMMIT 2
- * Patró aplicat: Replace Magic Number with Symbolic Constant.
- * Es defineixen constants amb nom per eliminar els literals numèrics fixos.
+ * CODI REFACTORITZAT - COMMIT 3
+ * Patró aplicat: Guard Clauses (Clàusules de Guarda).
+ * S'aïllen els casos especials al principi del bucle per eliminar niuaments.
  */
 class Magatzem {
     Article[] articles;
 
-    // PATRÓ: Replace Magic Number with Symbolic Constant
     public static final int MAX_QUALITAT = 50;
     public static final int QUALITAT_MINIMA = 0;
     public static final int LIMIT_DIES_URGENT = 11;
@@ -18,12 +17,19 @@ class Magatzem {
 
     public void actualitzarEstat() {
         for (int i = 0; i < articles.length; i++) {
+            
+            // PATRÓ: Clàusula de Guarda (Guard Clause)
+            // Si és el Martell de Thor, no canvia mai de qualitat ni de dies.
+            // Passem directament al següent article del magatzem, evitant comprovar la resta de 'ifs'.
+            if (articles[i].nom.equals("Martell de Thor (Llegendari)")) {
+                continue; 
+            }
+
+            // Flux principal (ja lliure de la condició del Martell de Thor)
             if (!articles[i].nom.equals("Formatge Gidurat")
                     && !articles[i].nom.equals("Entrades per al Concert del Trobador")) {
                 if (articles[i].qualitat > QUALITAT_MINIMA) {
-                    if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                        articles[i].qualitat = articles[i].qualitat - 1;
-                    }
+                    articles[i].qualitat = articles[i].qualitat - 1;
                 }
             } else {
                 if (articles[i].qualitat < MAX_QUALITAT) {
@@ -45,17 +51,14 @@ class Magatzem {
                 }
             }
 
-            if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                articles[i].diesPerVendre = articles[i].diesPerVendre - 1;
-            }
+            // Reduïm els dies per vendre (ja sabem segur que no és el Martell)
+            articles[i].diesPerVendre = articles[i].diesPerVendre - 1;
 
             if (articles[i].diesPerVendre < 0) {
                 if (!articles[i].nom.equals("Formatge Gidurat")) {
                     if (!articles[i].nom.equals("Entrades per al Concert del Trobador")) {
                         if (articles[i].qualitat > QUALITAT_MINIMA) {
-                            if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                                articles[i].qualitat = articles[i].qualitat - 1;
-                            }
+                            articles[i].qualitat = articles[i].qualitat - 1;
                         }
                     } else {
                         articles[i].qualitat = articles[i].qualitat - articles[i].qualitat;
