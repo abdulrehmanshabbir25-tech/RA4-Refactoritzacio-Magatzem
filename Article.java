2610d3c2af46cfabd737277eeb62f8d2e628a2a5
abdulrehmanshabbir25-tech <abdulrehman.shabbir.25@ibadia.cat>
2026-05-31 16:47:23 +0200

    Commit 9: Aplicar patró 'Move Method' per a la delegació de responsabilitats
    - S'ha aplicat el darrer patró del PDF movent els mètodes de comportament des de Magatzem cap a la classe Article.\n- S'elimina el 'Feature Envy' i es dota la classe de domini del seu propi estat, aconseguint un disseny net, modular, altament cohesionat i fàcil de mantenir.

--- Article.java
+++ Article.java
@@ -1,13 +1,19 @@
 /**
- * CLASSE MODEL REFACTORITZADA - COMMIT 8
- * Patró aplicat: Encapsulate Field.
- * Es canvien els atributs a 'private' i es generen getters i setters per protegir les dades.
+ * CLASSE MODEL REFACTORITZADA - COMMIT 9 (FINAL)
+ * Patró aplicat: Move Method.
+ * Es mou la lògica interna del comportament dels articles des de Magatzem cap a 
+ * la pròpia classe Article, on resideixen les dades originals segons el PDF.
  */
 class Article {
-    // PATRÓ: Encapsulate Field (Atributs privats)
     private String nom;
     private int diesPerVendre;
     private int qualitat;
+
+    // Constants mogudes al seu lloc corresponent de negoci
+    public static final int MAX_QUALITAT = 50;
+    public static final int QUALITAT_MINIMA = 0;
+    public static final int LIMIT_DIES_URGENT = 11;
+    public static final int LIMIT_DIES_CRITIC = 6;
 
     public Article(String nom, int diesPerVendre, int qualitat) {
         this.nom = nom;
@@ -15,28 +21,54 @@ class Article {
         this.qualitat = qualitat;
     }
 
-    // GETTERS I SETTERS
-    public String getNom() {
-        return nom;
+    // PATRÓ: Move Method (Lògica moguda aquí per evitar Feature Envy)
+    public boolean esArticleEstandard() {
+        return !this.nom.equals("Formatge Gidurat")
+                && !this.nom.equals("Entrades per al Concert del Trobador");
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
+    public void disminuirQualitatEstandard() {
+        if (this.qualitat > QUALITAT_MINIMA) {
+            this.qualitat = this.qualitat - 1;
+        }
     }
 
-    public int getDiesPerVendre() {
-        return diesPerVendre;
+    public void aumentarQualitatEspecials() {
+        if (this.qualitat >= MAX_QUALITAT) {
+            return; 
+        }
+
+        this.qualitat = this.qualitat + 1;
+
+        if (this.nom.equals("Entrades per al Concert del Trobador")) {
+            if (this.diesPerVendre < LIMIT_DIES_URGENT && this.qualitat < MAX_QUALITAT) {
+                this.qualitat = this.qualitat + 1;
+            }
+            if (this.diesPerVendre < LIMIT_DIES_CRITIC && this.qualitat < MAX_QUALITAT) {
+                this.qualitat = this.qualitat + 1;
+            }
+        }
     }
 
-    public void setDiesPerVendre(int diesPerVendre) {
-        this.diesPerVendre = diesPerVendre;
+    public void gestionarCaducitat() {
+        if (this.nom.equals("Formatge Gidurat")) {
+            if (this.qualitat < MAX_QUALITAT) {
+                this.qualitat = this.qualitat + 1;
+            }
+        } else if (this.nom.equals("Entrades per al Concert del Trobador")) {
+            this.qualitat = 0;
+        } else {
+            if (this.qualitat > QUALITAT_MINIMA) {
+                this.qualitat = this.qualitat - 1;
+            }
+        }
     }
 
-    public int getQualitat() {
-        return qualitat;
-    }
-
-    public void setQualitat(int qualitat) {
-        this.qualitat = qualitat;
-    }
+    // Getters i Setters (Commit 8)
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public int getDiesPerVendre() { return diesPerVendre; }
+    public void setDiesPerVendre(int diesPerVendre) { this.diesPerVendre = diesPerVendre; }
+    public int getQualitat() { return qualitat; }
+    public void setQualitat(int qualitat) { this.qualitat = qualitat; }
 }
--- Magatzem.java
+++ Magatzem.java
@@ -1,15 +1,10 @@
 /**
- * CODI REFACTORITZAT - COMMIT 8
- * Adaptació al patró Encapsulate Field.
- * Totes les consultes i modificacions d'atributs de l'article ara es fan mitjançant getters i setters.
+ * CODI REFACTORITZAT FINAL - COMMIT 9
+ * Classe Magatzem totalment alliberada de responsabilitats de baix nivell.
+ * Només s'encarrega d'iterar el magatzem i delegar el comportament a cada Article.
  */
 class Magatzem {
     Article[] articles;
-
-    public static final int MAX_QUALITAT = 50;
-    public static final int QUALITAT_MINIMA = 0;
-    public static final int LIMIT_DIES_URGENT = 11;
-    public static final int LIMIT_DIES_CRITIC = 6;
 
     public Magatzem(Article[] articles) {
         this.articles = articles;
@@ -18,63 +13,24 @@ class Magatzem {
     public void actualitzarEstat() {
         for (int i = 0; i < articles.length; i++) {
             
+            // Clàusula de Guarda (Commit 3)
             if (articles[i].getNom().equals("Martell de Thor (Llegendari)")) {
                 continue; 
             }
 
-            if (esArticleEstandard(articles[i])) {
-                disminuirQualitatEstandard(articles[i]);
+            // PATRÓ: Move Method (Cridem als mètodes que ara pertanyen a Article)
+            if (articles[i].esArticleEstandard()) {
+                articles[i].disminuirQualitatEstandard();
             } else {
-                aumentarQualitatEspecials(articles[i]);
+                articles[i].aumentarQualitatEspecials();
             }
 
-            // Ús de Getter i Setter per als dies
+            // Reduïm els dies per vendre de l'article
             articles[i].setDiesPerVendre(articles[i].getDiesPerVendre() - 1);
 
+            // Delegació final de la caducitat a l'objecte encarregat
             if (articles[i].getDiesPerVendre() < 0) {
-                gestionarCaducitat(articles[i]);
-            }
-        }
-    }
-
-    private boolean esArticleEstandard(Article article) {
-        return !article.getNom().equals("Formatge Gidurat")
-                && !article.getNom().equals("Entrades per al Concert del Trobador");
-    }
-
-    private void disminuirQualitatEstandard(Article article) {
-        if (article.getQualitat() > QUALITAT_MINIMA) {
-            article.setQualitat(article.getQualitat() - 1);
-        }
-    }
-
-    private void aumentarQualitatEspecials(Article article) {
-        if (article.getQualitat() >= MAX_QUALITAT) {
-            return; 
-        }
-
-        article.setQualitat(article.getQualitat() + 1);
-
-        if (article.getNom().equals("Entrades per al Concert del Trobador")) {
-            if (article.getDiesPerVendre() < LIMIT_DIES_URGENT && article.getQualitat() < MAX_QUALITAT) {
-                article.setQualitat(article.getQualitat() + 1);
-            }
-            if (article.getDiesPerVendre() < LIMIT_DIES_CRITIC && article.getQualitat() < MAX_QUALITAT) {
-                article.setQualitat(article.getQualitat() + 1);
-            }
-        }
-    }
-
-    private void gestionarCaducitat(Article article) {
-        if (article.getNom().equals("Formatge Gidurat")) {
-            if (article.getQualitat() < MAX_QUALITAT) {
-                article.setQualitat(article.getQualitat() + 1);
-            }
-        } else if (article.getNom().equals("Entrades per al Concert del Trobador")) {
-            article.setQualitat(0);
-        } else {
-            if (article.getQualitat() > QUALITAT_MINIMA) {
-                article.setQualitat(article.getQualitat() - 1);
+                articles[i].gestionarCaducitat();
             }
         }
     }
